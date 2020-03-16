d5d0d83809311f904f641e99a62a57ce3c8be716
Jayant Dhingra <jayantdhingra1441@gmail.com>
2020-03-16 16:32:59 +0530

    6th commit

---
+++ app/src/main/java/com/jayantx/messenger/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.jayantx.messenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            this.finish()
+        },500)
+    }
+}
